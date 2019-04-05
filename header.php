28714f449e1f35246cb482e0755796016447e0bf
Rachel Backert <rachel@rachelbackert.com>
2019-04-05 10:45:50 -0400

    styling for site title and tagline

--- header.php
+++ header.php
@@ -39,22 +39,24 @@
 			</nav>
 			<div class="site-branding">
 				<?php the_custom_logo(); ?>
-				<?php 
-				if ( is_front_page() && is_home() ) :
-					?>
-					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<?php
-				else :
-					?>
-					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php
-				endif;
+				<div class="site-meta">
+					<?php 
+					if ( is_front_page() && is_home() ) :
+						?>
+						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+						<?php
+					else :
+						?>
+						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+						<?php
+					endif;
 
-				$breadery_description = get_bloginfo( 'description', 'display' );
-				if ( $breadery_description || is_customize_preview() ) :
-					?>
-					<p class="site-description"><?php echo $breadery_description; /* WPCS: xss ok. */ ?></p>
-				<?php endif; ?>
+					$breadery_description = get_bloginfo( 'description', 'display' );
+					if ( $breadery_description || is_customize_preview() ) :
+						?>
+						<p class="site-description"><?php echo $breadery_description; /* WPCS: xss ok. */ ?></p>
+					<?php endif; ?>
+				</div>
 			</div><!-- .site-branding -->
 			<nav id="social-navigation" class="main-navigation navbar-collapse collapse justify-content-end">
 				<?php 
